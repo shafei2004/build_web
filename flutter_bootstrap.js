af8dc5748e44da171c14f27322d5cb73c3ef394a
Mohamed El Shafei <mohamedalielshafei7@gmail.com@example.com>
2025-10-19 19:57:16 +0300

    fix bugs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "533171681"
+    serviceWorkerVersion: "1428629084"
   }
 });
